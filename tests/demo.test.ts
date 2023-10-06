57ed80c05b543a592bce312b0891c42ca39a0e59
Gustavo Adolfo Loera Vazquez <124313842+gloera01@users.noreply.github.com>
2023-10-06 15:16:52 -0700

    Test deploy to dev (#3)
    * add temporal var log
    
    * fix typo?
    
    * fix errors
    
    * add pipe?
    
    * get branch name
    
    * add brnach name
    
    * test mapping environment
    
    * test without context
    
    * one more test with context job variable
    
    * other test
    
    * simplify mapping
    
    * changed
    
    * why
    
    * mamadas
    
    * bah
    
    * remove '
    
    * last test
    
    * sigh
    
    * test env mapping value
    
    * add env name
    
    * remove env name
    
    * add env name again
    
    * add test stage env
    
    * try to fix stage name
    
    * try other option
    
    * check ref name
    
    * try base ref
    
    * test push on features
    
    * try or on dev
    
    * test var
    
    * fix typo on or condition
    
    * test with no dynamic retrieval
    
    * test again
    
    * t
    
    * try on echo
    
    * fix typo
    
    * try 2
    
    * fix typo
    
    * final try
    
    * f
    
    * f?
    
    * last chance
    
    * add unit tests pipeline configuration
    
    * add some changes to test deploy
    
    * log env on test
    
    * use cross-env
    
    * remove cross-env command from pipeline
    
    * change service name
    
    ---------
    
    Co-authored-by: gus-tavo01 <gustavoa.loera01@gmail.com>

--- tests/demo.test.ts
+++ tests/demo.test.ts
@@ -8,6 +8,8 @@ describe('handlers/index', () => {
     // Action
     const result = await indexHandler(event);
 
+    console.log(`Env is: ${process.env.STAGE}`);
+
     // Assert
     expect(result).toHaveProperty('statusCode', 200);
   });
